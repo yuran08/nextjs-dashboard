75d3110444f119929845b125613b79b08155d265
yu_ran <2104208169@qq.com>
2025-03-02 20:02:44 +0800

    feat: add error handler & invoices contact & eslint

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -156,14 +156,12 @@ export async function fetchInvoiceById(id: string) {
 
     const invoice = data.map((invoice) => ({
       ...invoice,
-      // Convert amount from cents to dollars
       amount: invoice.amount / 100,
     }));
 
     return invoice[0];
   } catch (error) {
     console.error('Database Error:', error);
-    throw new Error('Failed to fetch invoice.');
   }
 }
 
--- app/seed/route.ts
+++ app/seed/route.ts
@@ -1,4 +1,4 @@
-import bcrypt from 'bcrypt';
+var bcrypt = require("bcryptjs");
 import postgres from 'postgres';
 import { invoices, customers, revenue, users } from '../lib/placeholder-data';
 
